fc0c5ff027098f56db6ce3d66792568045c60a7f
muzimu <2404580981@qq.com>
2025-10-15 00:26:04 +0800

    ✨ feat: s3上传文件

---
+++ s3.py
@@ -0,0 +1,18 @@
+import boto3
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+s3 = boto3.client('s3',
+                  region_name='cn-east-1', # 华东-浙江区 region id
+                  endpoint_url=getenv("s3_url"), # 华东-浙江区 endpoint
+                  aws_access_key_id=getenv("s3_ak"),
+                  aws_secret_access_key=getenv("s3_sk"),
+                  config=boto3.session.Config(signature_version="s3v4"))
+
+
+def upload_file(file_path: str):
+    file_name = file_path.split("/")[-1]
+    with open(file_path, 'rb') as f:
+        s3.put_object(Bucket='muzimu', Key='GiftBook/' + file_name, Body=f)
+        
+# upload_file('image/lzj-1.jpg')
