f189c0f3efeeb7eed1564e1cbf40a8a368de559a
muzimu <2404580981@qq.com>
2025-10-28 00:54:47 +0800

    🎈 perf: AI测试改用OpenAI

--- testai.py
+++ testai.py
@@ -1,26 +1,22 @@
 from os import getenv
 from dotenv import load_dotenv
-import requests
 load_dotenv()
-url = "https://openai.qiniu.com/v1/chat/completions"
-headers = {
-    "Authorization": "Bearer " + getenv("api_key"),
-    "Content-Type": "application/json"
-}
-payload = {
-    "stream": False,
-    "model": "moonshotai/kimi-k2-0905",
-    "messages": [
-        {
-            "role": "system",
-            "content": "You are a helpful assistant."
-        },
+from openai import OpenAI
+
+client = OpenAI(
+    api_key = getenv('api_key'),
+    base_url = getenv('base_url'),
+)
+
+messages = [
         {
             "role": "user",
-            "content": "Hello!"
+            "content": "hello",
         }
     ]
-}
+completion = client.chat.completions.create(
+        model=getenv('model'),
+        messages=messages,
+    )
 
-response = requests.post(url, json=payload, headers=headers)
-print(response.json())
+print(completion)
